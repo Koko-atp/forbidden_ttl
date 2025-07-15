40aa36b59b0a78c38c064726abed34beca3cad77
Koko-atp <koko.atp25@gmail.com>
2025-07-15 14:42:40 +0700

    git test 1

--- somerandomthing.py
+++ somerandomthing.py
@@ -3,6 +3,17 @@ class damn :
     age : None
     job : None
     hunger : int
+    class food :
+        burger : int
+        food : int
+        water : int
+        def __init__(self):
+            bruger = self.burger = 0    
+            food = self.food = 0    
+            water = self.water = 0    
+            pass
+
+        
     def __init__(self) :
         name = self. name = 'Unknow'
         age = self. age = 'Unknow'
@@ -11,15 +22,15 @@ class damn :
     def show(self):
         print(f'name : {self.name} \n age : {self.age} \n job : {self.job} \n Hunger : {self.hunger}%')
 
-    def eat(self):
-        self.food()
+    # def eat(self):
+    #     self.food.burger
 
-        print()
-        food = int(input('what to eat?'))
+    #     print()
+    #     food = int(input('what to eat?'))
                
-        print(f'you now aat a {food}')
+    #     print(f'you now aat a {food}')
 
-        self.hunger += 50
+    #     self.hunger += 50
     
     def menu(self) :
         self.show()
